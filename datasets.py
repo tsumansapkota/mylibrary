8ad73bb70b52de0a08054b47c3e114f067f17d89
Tsuman Sapkota <natokpas@gmail.com>
2021-07-15 19:33:30 +0545

    Added F-MNIST to datasets

--- datasets.py
+++ datasets.py
@@ -55,6 +55,91 @@ class MNIST:
         return mnist["training_images"], mnist["training_labels"], mnist["test_images"], mnist["test_labels"]
 
 
+class FashionMNIST:
+
+    def download_mnist(self, dirs="data/F-MNIST/"):
+        base_url = "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/"
+        
+        lib_dir = os.path.dirname(os.path.realpath(__file__))# + dirs
+        dirs = os.path.join(lib_dir,dirs) + '/'
+        
+        for name in filename:
+            print("Downloading " + name[1] + "...")
+            request.urlretrieve(base_url + name[1], dirs+name[1])
+        print("Download complete.")
+
+    def save_mnist(self, dirs="data/F-MNIST"):
+        lib_dir = os.path.dirname(os.path.realpath(__file__))# + dirs
+        dirs = os.path.join(lib_dir,dirs) + '/'
+        
+        mnist = {}
+        for name in filename[:2]:
+            with gzip.open(dirs + name[1], 'rb') as f:
+                mnist[name[0]] = np.frombuffer(f.read(), np.uint8, offset=16).reshape(-1, 28 * 28)
+        for name in filename[-2:]:
+            with gzip.open(dirs + name[1], 'rb') as f:
+                mnist[name[0]] = np.frombuffer(f.read(), np.uint8, offset=8)
+        with open(dirs + "fmnist.pkl", 'wb') as f:
+            pickle.dump(mnist, f)
+        print("Save complete.")
+
+    def init(self):
+        self.download_mnist()
+        self.save_mnist()
+
+    def load(self, dirs="data/F-MNIST"):
+        lib_dir = os.path.dirname(os.path.realpath(__file__))# + dirs
+        dirs = os.path.join(lib_dir,dirs) + '/'
+
+        with open(dirs + "fmnist.pkl", 'rb') as f:
+            mnist = pickle.load(f)
+        return mnist["training_images"], mnist["training_labels"], mnist["test_images"], mnist["test_labels"]
+
+
+# class FashionMNIST:
+
+#     def download_mnist(self, dirs="data/F-MNIST/"):
+#         base_url = "http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/"
+
+#         lib_dir = os.path.dirname(os.path.realpath(__file__))# + dirs
+#         dirs = os.path.join(lib_dir,dirs) + '/'
+
+#         for name in filename:
+#             print("Downloading " + name[1] + "...")
+#             request.urlretrieve(os.path.join(base_url, name[1]), os.path.join(dirs, name[1]))
+#         print("Download complete.")
+
+#     def save_mnist(self, dirs="data/F-MNIST"):
+#         # lib_dir = os.path.dirname(os.path.realpath(__file__))# + dirs
+#         # dirs = os.path.join(lib_dir,dirs) + '/'
+
+#         mnist = {}
+#         for name in filename[:2]:
+#             with gzip.open(os.path.join(dirs, name[1]), 'rb') as f:
+#                 mnist[name[0]] = np.frombuffer(f.read(), np.uint8, offset=16).reshape(-1, 28 * 28)
+#         for name in filename[-2:]:
+#             with gzip.open(os.path.join(dirs, name[1]), 'rb') as f:
+#                 mnist[name[0]] = np.frombuffer(f.read(), np.uint8, offset=8)
+
+#         with open(os.path.join(dirs, "fmnist.pkl"), 'wb') as f:
+#             pickle.dump(mnist, f)
+#         print("Save complete.")
+
+#     def init(self):
+#         self.download_mnist()
+#         self.save_mnist()
+
+#     def load(self, dirs="data/F-MNIST"):
+#         # lib_dir = os.path.dirname(os.path.realpath(__file__))# + dirs
+#         # dirs = os.path.join(lib_dir,dirs) + '/'
+#         if not os.path.exists(os.path.join(dirs, "fmnist.pkl")):
+#             self.download_mnist(dirs)
+#             self.save_mnist(dirs)
+#         with open(os.path.join(dirs, "fmnist.pkl"), 'rb') as f:
+#             mnist = pickle.load(f)
+#         return mnist["training_images"], mnist["training_labels"], mnist["test_images"], mnist["test_labels"]
+
+
 class CurveTest:
 
     def __init__(self):
